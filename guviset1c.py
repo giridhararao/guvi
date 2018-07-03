53d4e9d212b8e4dbce5043b89ed3117dda6a8dfb
giridhararao <www.giridhararao@gmail.com>
2018-07-03 22:33:14 +0530

    Create guviset1c.py

---
+++ guviset1c.py
@@ -0,0 +1,7 @@
+a=input()
+if a in ('a','e','i','o','u'):
+   print("Vowel")
+elif a in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+   print("Consonant")
+else:
+  print("invalid")
