b5a2f21ff85a5bc59b6b59d7e564c51fcd2776f0
M.Giridhara Rao <www.giridhararao@gmail.com>
2019-02-09 20:11:47 +0530

    Create mink.py

---
+++ mink.py
@@ -0,0 +1,23 @@
+import sys,string
+def freq(s):
+    dic1 = {}
+    for c in s :
+        dic1[c] = dic1.get(c,0) + 1
+    return dic1
+s = input()
+n = len(s)
+dic1 = freq(s)
+Lk = list(dic1.keys())
+for j in range(n-2,-1,-1) :
+    for c in Lk :
+        k = 0
+        for i in range(0,n-j) :
+            li, ri = i,j+i
+            s2 = s[li:ri + 1]
+            if(c in s2):
+                k += 1
+        if(k == n-j):
+            c2 = c
+            k2 = k
+            out= j+1
+print(out)
