2232643322ac590905cfb43851b8435fe3b0db8f
M.Giridhara Rao <www.giridhararao@gmail.com>
2019-03-26 06:56:25 +0530

    Update charchan.py

--- charchan.py
+++ charchan.py
@@ -10,10 +10,10 @@ for j in range(n-1,-1,-1) :
         for j in range(k,0,-1) :
             s21 = 'ab'*j
             s22 = s21 + 'a'
-            if s22 in s :
+            if s22 in a :
                 print(len(s22))
                 sys.exit()
-            elif s21 in s :
+            elif s21 in a :
                 print(len(s21))
                 sys.exit()
 print(0)
