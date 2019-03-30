e67e245a2b31463f39087430bce0f7cf5bc92b4a
M.Giridhara Rao <www.giridhararao@gmail.com>
2019-03-30 12:22:05 +0530

    Update maxdupli.py

--- maxdupli.py
+++ maxdupli.py
@@ -2,7 +2,7 @@ a = input()
 for i in range(11,100,11) :
     a2 = str(i)
     if a2 in a :
-        i2 = a.index(s2)
+        i2 = a.index(a2)
         a3 = a[:i2] + a2[0] + a[i2+2:]
         print(a3)
         break
